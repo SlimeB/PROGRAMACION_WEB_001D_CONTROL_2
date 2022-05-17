5a224ddd726172affd816aa80bab7e69d04ea057
Gabriel Lacunza <gab.lacunza@gmail.com>
2022-05-17 18:22:15 -0400

    completo

--- js/validacion_login.js
+++ js/validacion_login.js
@@ -5,9 +5,9 @@ $('#login').validate({
         },
         "constraseña":{
             required: true
-        }
+        },
     },
-    "message": {
+    messages: {
         "correo":{
             required: "Correo es un campo obligatorio",
         },
@@ -16,6 +16,51 @@ $('#login').validate({
         }
     },
 });
+
+function validateEmail(email) {
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
+}
+
+$.validator.addMethod(
+    "validateemail",
+    function(value, element, validate) {
+        debugger
+        if (validate) {
+            return validateEmail(value);
+        }
+    },
+    "Formato de correo incorrecto"
+);
+
+$.validator.addMethod(
+    "onenumber",
+    function(value, element, validate) {
+        if (validate) {
+            var re = new RegExp('.*[0-9].*');
+            resp = re.test(value);
+            return resp;
+        }
+    },
+    "La contraseña debe contener al menos un número"
+);
+
+$.validator.addMethod(
+    "onemayus",
+    function(value, element, validate) {
+        if (validate) {
+            var re = new RegExp('.*[A-Z].*');
+            resp = re.test(value);
+            return resp;
+        }
+    },
+    "La contraseña debe contener al menos una mayúscula"
+);
+
+$("#correo").rules("add", { validateemail: true });
+$("#password").rules("add", { onenumber: true });
+$("#password").rules("add", { onemayus: true });
+
 /*
 let btn = document.getElementById("validacion");
 btn.addEventListener("click", function() {
